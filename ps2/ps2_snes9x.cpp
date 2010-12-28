205fe348cb8b846786c28541577c3e1c456d9d60
ragnarok2040 <ragnarok2040@gmail.com>
2010-12-28 16:57:15 -0500

    Commit current version of ps2_snes9x.cpp before adding UI

--- ps2/ps2_snes9x.cpp
+++ ps2/ps2_snes9x.cpp
@@ -1,11 +1,21 @@
-// Standard headers
+// C++ standard headers
+#include <iostream>
+
+// C Standard headers
 #include <stdio.h>
 #include <string.h>
 #include <libgen.h>
 #include <sys/stat.h>
+#include <time.h>
 
 // ps2sdk headers
+#include <audsrv.h>
 #include <fileXio_rpc.h>
+#include <dma.h>
+#include <packet.h>
+#include <gs_psm.h>
+#include <graph.h>
+#include <draw.h>
 
 // Ported headers
 #include <zlib.h>
@@ -16,6 +26,7 @@
 #include <settings.h>
 #endif
 
+#include <video.h>
 #include <init.h>
 #include <paths.h>
 
@@ -26,41 +37,104 @@
 #include <apu.h>
 #include <controls.h>
 
-
 #include <snes9x_cfg.h>
+#include <ps2_controls.h>
+#include <ps2_video.h>
+#include <ps2_sound_driver_audsrv.h>
 
-unsigned int palette[256];
-
-// miscellaneous paths
-static char misc[PATH_MAX + 1];
+//int syncing = 0;
 
 // Needs to be global
 #if 0
 settings_t *settings;
 #endif
 
+void make_s9x_dirs();
+
+bool8 S9xInitSoundDevice(void);
+
+void S9xSyncSpeedFinish();
+
 int main(int argc, char **argv)
 {
+	int last_ms = 0;
+	int current_ms;
+	int frames = 0;
 
-	config_t *cfg;
+	// hardcoded dir
+	char dir[50] = "mc0:/SYS-CONF";
 
-	init_bios_modules();
+	std::ios::sync_with_stdio(false);
+
+	reset_iop();
+
+	init_basic_modules(NULL);
+
+	dma_channel_initialize(DMA_CHANNEL_GIF,NULL,0);
+	dma_channel_fast_waits(DMA_CHANNEL_GIF);
 
 	S9xInitSettings();
 
-	cfg = cfg_open("mc0:/SYS-CONF/snes9x.cfg");
+	init_sound_modules(NULL);
 
-	S9xParseCFG(cfg);
+	// hardcoded dir
+	S9xLoadCFG("mc0:/SYS-CONF/snes9x.cfg");
 
-	cfg_close(cfg);
+	// hardcoded dir
+	ps2_input_cfg("mc0:/SYS-CONF/snes9x.cfg");
 
-	S9xSaveSettingsToCFG("mc0:/SYS-CONF/snes9x.cfg");
+	ps2_input_open_pads();
+
+	make_s9x_dirs();
+
+	if (!Memory.Init () || !S9xInitAPU ())
+	{
+		printf("Out of Memory\n");
+	}
+
+	S9xInitGFX();
+
+	S9xInitSoundDevice();
+
+	S9xReportControllers();
+
+	S9xSetRenderPixelFormat(BGR555);
+
+	if (!S9xGraphicsInit())
+	{
+		printf("Out of Memory\n");
+	}
+
+	// hardcoded rom
+
+	//if (!Memory.LoadROM("mc0:SYS-CONF/rom.smc"))
+	//if (!Memory.LoadROM("mc0:smrpg_us.smc"))
+	if (!Memory.LoadROM("mc0:/SYS-CONF/SuperMar.smc"))
+	{
+		printf("Something's wrong\n");
+		SleepThread();
+	}
+
+	Memory.LoadSRAM(S9xGetFilename(".srm", SRAM_DIR));
+
+	while (1)
+	//for (i = 0; i < 200; i++)
+	{
+
+		S9xMainLoop();
+
+		//printf("samples = %d\n",S9xGetSampleCount());
+		ps2_input_report_pads();
+
+	}
+
+	//exit(0);
 
 	return 0;
 }
 
-// conffile.h
 #ifdef CONFFILE_SUPPORT
+// conffile.h
 void S9xParsePortConfig(ConfigFile &, int pass)
 {
 	return;
@@ -75,18 +149,19 @@ void S9xHandlePortCommand (s9xcommand_t cmd, int16 data1, int16 data2)
 
 bool S9xPollButton (uint32 id, bool *pressed)
 {
-	return true;
+	return false;
 }
 
 bool S9xPollPointer (uint32 id, int16 *x, int16 *y)
 {
-	return true;
+	return false;
 }
 
 bool S9xPollAxis (uint32 id, int16 *value)
 {
-	return true;
+	return false;
 }
+
 bool8 S9xOpenSnapshotFile (const char *filepath, bool8 read_only, STREAM *file)
 {
 
@@ -94,28 +169,28 @@ bool8 S9xOpenSnapshotFile (const char *filepath, bool8 read_only, STREAM *file)
 
 	if ((filepath == NULL) || (file == NULL))
 	{
-		return 0;
+		return FALSE;
 	}
 
 	if (read_only)
 	{
-		getfile = gzopen(filepath,"r");
+		getfile = OPEN_STREAM(filepath,"r");
 	}
 	else
 	{
-		getfile = gzopen(filepath,"w6h");
+#ifdef ZLIB
+		getfile = OPEN_STREAM(filepath,"w6h");
+#else
+		getfile = OPEN_STREAM(filepath,"w");
+#endif
 	}
 
-	if (getfile == Z_NULL)
+	if ((getfile == Z_NULL) || (getfile == NULL))
 	{
-		return 0;
-	}
-	else
-	{
-		return 1;
+		return FALSE;
 	}
 
-	return 0;
+	return TRUE;
 
 }
 
@@ -131,12 +206,7 @@ void S9xExit (void)
 
 bool8 S9xInitUpdate (void)
 {
-	return 1;
-}
-
-bool8 S9xDeinitUpdate (int width, int height)
-{
-	return 1;
+	return TRUE;
 }
 
 void S9xMessage (int type, int number, const char *message)
@@ -144,9 +214,39 @@ void S9xMessage (int type, int number, const char *message)
 	printf("%s\n",message);
 }
 
+S9xSoundDriver *driver;
+
+bool8 S9xInitSoundDevice(void)
+{
+
+	driver = new S9xAudsrvSoundDriver();
+
+	if (driver != NULL)
+	{
+
+		driver->init();
+
+		// hardcoded buffer_ms, lag_ms
+		S9xInitSound(96,0);
+
+		S9xSetSoundMute(FALSE);
+
+	}
+	else
+	{
+
+		S9xSetSoundMute(TRUE);
+
+	}
+
+	return TRUE;
+}
+
 bool8 S9xOpenSoundDevice (void)
 {
-	return 1;
+
+	return driver->open_device ();
+
 }
 
 const char	dir_types[13][32] =
@@ -166,17 +266,16 @@ const char	dir_types[13][32] =
 	""				// LAST_DIR
 };
 
-#if 0
+
 void make_s9x_dirs(void)
 {
+
 	int i;
-	const char *path;
-	iox_stat_t stat;
-	int filemode =  FIO_S_IRUSR | FIO_S_IWUSR | FIO_S_IXUSR | FIO_S_IRGRP | FIO_S_IWGRP | FIO_S_IXGRP | FIO_S_IROTH | FIO_S_IWOTH | FIO_S_IXOTH;
 
 	for (i = 0; i < LAST_DIR; i++)
 	{
 
+		// DEFAULT_DIR is the same as these
 		if (i == HOME_DIR)
 		{
 			continue;
@@ -186,30 +285,30 @@ void make_s9x_dirs(void)
 			continue;
 		}
 
-		path = S9xGetDirectory(i);
-
-		if (fileXioGetStat(path,&stat) < 0)
-		{
-			fileXioMkDir(path,filemode);
-		}
+		fileXioMkdir(S9xGetDirectory((s9x_getdirtype)i),0666);
 
 	}
 
 }
-#endif
 
 const char *S9xGetDirectory (enum s9x_getdirtype dirtype)
 {
-#if 0
-	strcpy(misc,settings->home);
+	static char directory[PATH_MAX+1];
+
+	//strcpy(directory,settings->home);
+	strcpy(directory,"mc0:/SYS-CONF");
 
 	if (dir_types[dirtype][0])
 	{
-		strcat(misc,"/");
-		strcat(misc,dir_types[dirtype]);
-	}
+#if 0
+		//sprintf(directory,"%s/%s",settings->home,dir_types[dirtype]);
 #endif
-	return misc;
+		sprintf(directory,"%s/%s","mc0:/SYS-CONF",dir_types[dirtype]);
+		//strcat(directory,"/");
+		//strcat(directory,dir_types[dirtype]);
+	}
+	printf("directory = %s\n",directory);
+	return directory;
 
 }
 
@@ -217,6 +316,7 @@ const char *S9xGetFilename (const char *extension, enum s9x_getdirtype dirtype)
 {
 
 	// directory
+	static char filename[PATH_MAX+1];
 	char directory[PATH_MAX+1];
 
 	// components
@@ -228,15 +328,16 @@ const char *S9xGetFilename (const char *extension, enum s9x_getdirtype dirtype)
 	strcpy(directory,S9xGetDirectory(dirtype));
 	_splitpath(Memory.ROMFilename, drive, dir, fname, ext);
 
-	snprintf(misc, PATH_MAX + 1, "%s%s%s%s", directory, SLASH_STR, fname, extension);
+	snprintf(filename, PATH_MAX + 1, "%s%s%s%s", directory, SLASH_STR, fname, extension);
 
-	return misc;
+	return filename;
 
 }
 
 const char *S9xGetFilenameInc (const char *extension, enum s9x_getdirtype dirtype)
 {
-#if 0
+
+	static char filename[PATH_MAX+1];
 	iox_stat_t stat;
 	int i = 0;
 
@@ -251,16 +352,16 @@ const char *S9xGetFilenameInc (const char *extension, enum s9x_getdirtype dirtyp
 
 	do
 	{
-		snprintf(misc, PATH_MAX + 1, "%s%s%s.%03d%s", directory, SLASH_STR, fname, i++, extension);
+		snprintf(filename, PATH_MAX + 1, "%s%s%s.%03d%s", directory, SLASH_STR, fname, i++, extension);
 
 		if (i > 100)
 		{
 			break;
 		}
 	}
-	while (!fileXioGetStat(misc, &stat));
-#endif
-	return misc;
+	while (!fileXioGetStat(filename, &stat));
+
+	return filename;
 
 }
 
@@ -305,12 +406,365 @@ void S9xSetPalette (void)
 {
 }
 
-void S9xSyncSpeed (void)
+static inline int __udelay(unsigned int usecs)
 {
+ 
+	register unsigned int loops_total = 0;
+	register unsigned int loops_end   = usecs * 148;
+
+	if (usecs > loops_end)
+	{
+
+		return -1;
+
+	}
+
+	asm volatile (".set noreorder\n\t"
+				  "0:\n\t"
+				  "beq %0,%2,0f\n\t"
+				  "addiu %0,1\n\t"
+				  "bne %0,%2,0b\n\t"
+				  "addiu %0,1\n\t"
+				  "0:\n\t"
+				  ".set reorder\n\t"
+				  :"=r" (loops_total)
+				  :"0" (loops_total), "r" (loops_end));
+
+	return 0;
+
 }
 
-// gfx.h
-bool8 S9xContinueUpdate (int width, int height)
+void S9xSyncSpeed (void)
 {
-	return 1;
+
+	if (Settings.SoundSync)
+	{
+		while (!S9xSyncSound());
+			__udelay(0);
+	}
+
+	if (Settings.DumpStreams)
+		return;
+
+#ifdef NETPLAY_SUPPORT
+	if (Settings.NetPlay && NetPlay.Connected)
+	{
+	#if defined(NP_DEBUG) && NP_DEBUG == 2
+		printf("CLIENT: SyncSpeed @%d\n", S9xGetMilliTime());
+	#endif
+
+		S9xNPSendJoypadUpdate(old_joypads[0]);
+		for (int J = 0; J < 8; J++)
+			joypads[J] = S9xNPGetJoypad(J);
+
+		if (!S9xNPCheckForHeartBeat())
+		{
+			NetPlay.PendingWait4Sync = !S9xNPWaitForHeartBeatDelay(100);
+		#if defined(NP_DEBUG) && NP_DEBUG == 2
+			if (NetPlay.PendingWait4Sync)
+				printf("CLIENT: PendingWait4Sync1 @%d\n", S9xGetMilliTime());
+		#endif
+
+			IPPU.RenderThisFrame = TRUE;
+			IPPU.SkippedFrames = 0;
+		}
+		else
+		{
+			NetPlay.PendingWait4Sync = !S9xNPWaitForHeartBeatDelay(200);
+		#if defined(NP_DEBUG) && NP_DEBUG == 2
+			if (NetPlay.PendingWait4Sync)
+				printf("CLIENT: PendingWait4Sync2 @%d\n", S9xGetMilliTime());
+		#endif
+
+			if (IPPU.SkippedFrames < NetPlay.MaxFrameSkip)
+			{
+				IPPU.RenderThisFrame = FALSE;
+				IPPU.SkippedFrames++;
+			}
+			else
+			{
+				IPPU.RenderThisFrame = TRUE;
+				IPPU.SkippedFrames = 0;
+			}
+		}
+
+		if (!NetPlay.PendingWait4Sync)
+		{
+			NetPlay.FrameCount++;
+			S9xNPStepJoypadHistory();
+		}
+
+		return;
+	}
+#endif
+
+	if (Settings.HighSpeedSeek > 0)
+		Settings.HighSpeedSeek--;
+
+	if (Settings.TurboMode)
+	{
+		if ((++IPPU.FrameSkip >= Settings.TurboSkipFrames) && !Settings.HighSpeedSeek)
+		{
+			IPPU.FrameSkip = 0;
+			IPPU.SkippedFrames = 0;
+			IPPU.RenderThisFrame = TRUE;
+		}
+		else
+		{
+			IPPU.SkippedFrames++;
+			IPPU.RenderThisFrame = FALSE;
+		}
+
+		return;
+	}
+
+	static unsigned long next_ms = 0;
+	static unsigned int next_sec = 0;
+
+	unsigned long now_ms;
+	unsigned int  now_sec;
+
+	now_ms = (unsigned long)(((float)clock()) / 576.0f);
+	now_sec = (unsigned int)(((float)now_ms) / 1000.0f);
+
+	// If there is no known "next" frame, initialize it now.
+	if (next_sec == 0)
+	{
+		next_sec = now_sec;
+		next_ms = now_ms;
+		next_ms++;
+	}
+
+	// If we're on AUTO_FRAMERATE, we'll display frames always only if there's excess time.
+	// Otherwise we'll display the defined amount of frames.
+	unsigned	limit = (Settings.SkipFrames == AUTO_FRAMERATE) ? (next_ms < now_ms ? 10 : 1) : Settings.SkipFrames;
+
+	IPPU.RenderThisFrame = (++IPPU.SkippedFrames >= limit) ? TRUE : FALSE;
+
+	if (IPPU.RenderThisFrame)
+		IPPU.SkippedFrames = 0;
+	else
+	{
+		// If we were behind the schedule, check how much it is.
+		if (next_ms < now_ms)
+		{
+			unsigned lag = (now_sec - next_sec) * 1000 + now_ms - next_ms;
+
+			if (lag >= 500)
+			{
+				// More than a half-second behind means probably pause.
+				// The next line prevents the magic fast-forward effect.
+				next_sec = now_sec;
+				next_ms = now_ms;
+			}
+		}
+	}
+
+	// Delay until we're completed this frame.
+	while (next_ms > now_ms)
+	{
+		unsigned timeleft = ((next_sec - now_sec) * 1000 + next_ms - now_ms)*1000;
+
+		// If we're ahead of time, sleep a while.
+		__udelay(timeleft);
+
+		now_ms = (unsigned long)(((float)clock())/576.0f);
+		now_sec = (unsigned int)(((float)now_ms) / 1000.0f);
+
+	}
+
+	// Calculate the timestamp of the next frame.
+	next_ms += (unsigned long)(((float)Settings.FrameTime)/1000.0f);
+
+	if (next_ms >= 1000)
+	{
+		next_sec += (unsigned int)(((float)next_ms) / 1000.0f);
+		next_ms %= 1000;
+	}
+
 }
+
+/*
+unsigned long now_ms;
+unsigned int  now_sec;
+
+static unsigned long next_ms = 0;
+static unsigned int next_sec = 0;
+
+#undef TIMER_DIFF
+#define TIMER_DIFF(a, b) (long)(((a##_sec - b##_sec) * 1000) + a##_ms - b##_ms)
+void S9xSyncSpeedFinish (void)
+{
+	if (!syncing)
+		return;
+
+	now_ms = (unsigned long)(((float)clock()) / 576.0f);
+	now_sec = (unsigned int)(((float)now_ms) / 1000.0f);
+
+	if (Settings.SoundSync)
+	{
+		while (!S9xSyncSound ())
+		{
+			__udelay(100);
+
+			next_ms = (unsigned long)(((float)clock()) / 576.0f);
+			next_sec = (unsigned int)(((float)now_ms) / 1000.0f);
+
+			// If we can't sync sound within a second, we're probably deadlocked
+			if (TIMER_DIFF(next,now) > 1000)
+			{
+				// Flush out our sample buffer and give up.
+				S9xClearSamples ();
+
+				break;
+			}
+		}
+
+		next_ms = now_ms;
+		next_sec = now_sec;
+
+		return;
+	}
+
+	if (TIMER_DIFF (next, now) < -500)
+	{
+		next_ms = now_ms;
+		next_sec = now_sec;
+	}
+
+	while (next_ms > now_ms)
+	{
+		int time_left = TIMER_DIFF (next, now);
+
+		if (time_left > 500)
+		{
+			next_ms = now_ms;
+			next_sec = now_sec;
+			break;
+		}
+
+		__udelay(time_left);
+
+		now_ms = (unsigned long)(((float)clock()) / 576.0f);
+		now_sec = (unsigned int)(((float)now_ms) / 1000.0f);
+	}
+
+	next_ms += (unsigned long)(((float)Settings.FrameTime)/1000.0f);
+
+	if (next_ms >= 1000)
+	{
+		next_sec += (unsigned int)(((float)next_ms) / 1000.0f);
+		next_ms %= 1000;
+	}
+
+	syncing = 0;
+
+	return;
+}
+
+void S9xSyncSpeed (void)
+{
+
+	unsigned int limit;
+	int          lag;
+	int          frametime;
+
+
+
+#ifdef NETPLAY_SUPPORT
+	if (S9xNetplaySyncSpeed())
+		return;
+#endif
+
+
+	if (Settings.HighSpeedSeek > 0)
+	{
+		Settings.HighSpeedSeek--;
+		IPPU.RenderThisFrame = FALSE;
+		IPPU.SkippedFrames = 0;
+
+		now_ms = (unsigned long)(((float)clock()) / 576.0f);
+		now_sec = (unsigned int)(((float)now_ms) / 1000.0f);
+
+		next_ms = now_ms;
+		next_sec = now_sec;
+
+		syncing = 0;
+
+		return;
+	}
+
+	if (Settings.TurboMode)
+	{
+		if ((++IPPU.FrameSkip >= Settings.TurboSkipFrames) && !Settings.HighSpeedSeek)
+		{
+			IPPU.FrameSkip = 0;
+			IPPU.SkippedFrames = 0;
+			IPPU.RenderThisFrame = TRUE;
+		}
+		else
+		{
+			IPPU.SkippedFrames++;
+			IPPU.RenderThisFrame = FALSE;
+		}
+
+		return;
+	}
+
+	now_ms = (unsigned long)(((float)clock()) / 576.0f);
+	now_sec = (unsigned int)(((float)now_ms) / 1000.0f);
+
+	// If there is no known "next" frame, initialize it now.
+	if (next_sec == 0)
+	{
+		next_sec = now_sec;
+		next_ms = now_ms+1;
+	}
+
+	if (Settings.SkipFrames == AUTO_FRAMERATE && !Settings.SoundSync)
+	{
+		lag = TIMER_DIFF(now,next);
+		frametime = (int)(((float)Settings.FrameTime)/1000.0f);
+
+		if (lag > frametime)
+		{
+			if (lag > (frametime * 10))
+			{
+				next_ms = now_ms;
+				next_sec = now_sec;
+				IPPU.RenderThisFrame = 1;
+				IPPU.SkippedFrames = 0;
+			}
+			else
+			{
+				IPPU.RenderThisFrame = 0;
+				IPPU.SkippedFrames++;
+			}
+		}
+		else
+		{
+			IPPU.RenderThisFrame = 1;
+			IPPU.SkippedFrames = 0;
+		}
+	}
+	else
+	{
+		limit = Settings.SoundSync ? 1 : Settings.SkipFrames + 1;
+
+		IPPU.SkippedFrames++;
+		IPPU.RenderThisFrame = 0;
+
+		if (IPPU.SkippedFrames >= limit)
+		{
+			IPPU.RenderThisFrame = 1;
+			IPPU.SkippedFrames = 0;
+		}
+	}
+
+	syncing = 1;
+
+	return;
+
+}
+*/
+
